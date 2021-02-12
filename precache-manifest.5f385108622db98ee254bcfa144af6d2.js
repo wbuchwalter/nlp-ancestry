1e303401adc92d5303e678d215453a68c2705216
William Buchwalter <wbuchwalter@gmail.com>
2021-02-11 23:52:53 -0500

    Updates

---
+++ precache-manifest.5f385108622db98ee254bcfa144af6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09bc6b3ed4e81feb561b97184f63c08",
+    "url": "/nlp-ancestry/index.html"
+  },
+  {
+    "revision": "de96e4bbd70effc5a451",
+    "url": "/nlp-ancestry/static/css/main.ad6229a9.chunk.css"
+  },
+  {
+    "revision": "b89d45ee1be0a5a72384",
+    "url": "/nlp-ancestry/static/js/2.4688a788.chunk.js"
+  },
+  {
+    "revision": "5315c0d0e8a3429010fd824765812b6b",
+    "url": "/nlp-ancestry/static/js/2.4688a788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de96e4bbd70effc5a451",
+    "url": "/nlp-ancestry/static/js/main.fafd9834.chunk.js"
+  },
+  {
+    "revision": "8c3f7b4ac9353825d96f",
+    "url": "/nlp-ancestry/static/js/runtime-main.125c5f58.js"
+  }
+]);
